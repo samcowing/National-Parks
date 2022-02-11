ecca0609d092fff52407a0bc104ab21bb895c0f4
sam-cowing <sam.ryan.cowing@gmail.com>
2022-02-11 13:14:55 -0500

    replace state input with a dropdown

--- national-parks-frontend/src/components/SearchIndex.jsx
+++ national-parks-frontend/src/components/SearchIndex.jsx
@@ -1,8 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import StateList from '../util/StateList'
+
 
 function SearchIndex() {
-    const [state, setState] = useState("")
+    const [state, setState] = useState("AL")
 
     const handleChange = (e) => {
         const searchedState = e.target.value 
@@ -14,13 +16,9 @@ function SearchIndex() {
             <h1>Homepage</h1>
             <h4>Enter a state</h4>
             <form className="header__form">
-                <input
-                    className="form__input-text"
-                    name="name"
-                    placeholder="search by state"
-                    value={state}
-                    onChange={handleChange}
-                />
+                <select onChange={handleChange}>
+                    <StateList />
+                </select>
                 <Link to={`/parks/` + state}>
                     <button>Find parks</button>
                 </Link>
