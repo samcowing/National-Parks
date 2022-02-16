7ba05e7e857f734a36d9a3b71cf032b20fba77b1
sam-cowing <sam.ryan.cowing@gmail.com>
2022-02-16 11:38:42 -0500

    homepage styled

--- national-parks-frontend/src/components/SearchIndex.jsx
+++ national-parks-frontend/src/components/SearchIndex.jsx
@@ -16,10 +16,14 @@ function SearchIndex() {
             <div className='search-page-container'>
                 <div className='search-container'>
                     <div className='search-box'>
-                        <h1>Homepage</h1>
-                        <h4>Enter a state</h4>
+                        <h1>National Parks</h1>
+                        <p>by Sam Cowing</p>
+                        <h4>
+                            Explore Nationation Parks within the U.S. by selecting a state in which to start exploring below. All park data is provided by the NPS.
+                        </h4>
+                        <h6>Enter a state</h6>
                         <form className="header__form">
-                            <select onChange={handleChange}>
+                            <select onChange={handleChange} placeholder="Select A State">
                                 <StateList />
                             </select>
                             <Link to={`/parks/` + state}>
