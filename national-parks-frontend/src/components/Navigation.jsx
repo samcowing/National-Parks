a87c4135700ef4f444cf74952290f1492e2f0ead
sam-cowing <sam.ryan.cowing@gmail.com>
2022-05-10 22:44:56 -0400

    updated nav to be more responsive friendly

--- national-parks-frontend/src/components/Navigation.jsx
+++ national-parks-frontend/src/components/Navigation.jsx
@@ -1,17 +1,43 @@
 import { Link } from 'react-router-dom'
 import logo from '../logo/S.mp4'
 import ReactPlayer from 'react-player'
+import React, { useState, useEffect } from 'react';
 
 function Navigation() {
+    const [show, setShow] = useState(true);
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    const controlNavbar = () => {
+        if (typeof window !== 'undefined') { 
+          if (window.scrollY > lastScrollY) { 
+            setShow(true); 
+          } else { 
+            setShow(false);  
+          }
+          setLastScrollY(window.scrollY); 
+        }
+      };
+    
+      useEffect(() => {
+        if (typeof window !== 'undefined') {
+          window.addEventListener('scroll', controlNavbar);
+
+          return () => {
+            window.removeEventListener('scroll', controlNavbar);
+          };
+        }
+      }, [lastScrollY]);
+
     return (
-        <nav className="nav">
+        <nav className={`active ${show && 'hidden'}`}>
             <Link to={`/`}>
                 <ReactPlayer
                     url = {logo} 
                     playing = {true}
+                    className = {'react-player'}
                     muted = {true}
-                    width = { "180px" }
-                    height = { "180px" }
+                    width = {"180px"}
+                    height = {"180px"}
                 />
             </Link>
         </nav>
